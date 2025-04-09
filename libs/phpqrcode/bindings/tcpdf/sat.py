0c207a3a51a8be8479ca108a71a8cf9c6fba22db
Sathwika1026 <sathwikasavukari@gmail.com>
2025-04-09 15:52:53 +0530

    updated

---
+++ libs/phpqrcode/bindings/tcpdf/sat.py
@@ -0,0 +1,11 @@
+import turtle
+t=turtle.Turtle()
+for outer_loop in range(4):
+    for inner_loop in range(4):
+        t.forward(50)
+        t.rt(90)
+    t.forward(100)
+for last_loop in range(4):
+        t.forward(50)
+        t.rt(90)
+turtle.mainloop()
